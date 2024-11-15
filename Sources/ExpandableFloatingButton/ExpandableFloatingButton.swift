3866139d85aae2375a8dda6647dbaf2a806ea10d
watakazu-git <132337028+watakazu-git@users.noreply.github.com>
2024-11-15 16:15:35 +0900

    Update: Add Comment

--- Sources/ExpandableFloatingButton/ExpandableFloatingButton.swift
+++ Sources/ExpandableFloatingButton/ExpandableFloatingButton.swift
@@ -2,73 +2,99 @@ import SwiftUI
 
 @available(iOS 18.0, macOS 14.0, *)
 @MainActor
+/// A customizable Expandable Floating Button component for SwiftUI.
 public struct ExpandableFloatingButton: View {
-    // MARK:  Internal Properties
-    @State var isPushed: Bool = false
-    var isGradient: Bool = true
+    // MARK: Internal Properties
+    @State var isExpanded: Bool = false
+    let usesGradient: Bool
 
     // MARK: Argument Properties
-    // Systemname
-    var mainSystemName: String
-    var firstSystemName: String
-    var secondSystemName: String
-    var thirdSystemName: String
+    /// The system name for the main button icon.
+    let mainIconName: String
+    /// The system name for the first expandable button icon.
+    let firstButtonIconName: String
+    /// The system name for the second expandable button icon.
+    let secondButtonIconName: String
+    /// The system name for the third expandable button icon.
+    let thirdButtonIconName: String
 
-    // Color
-    var mainButtonColor: Color
-    var firstButtonColor: Color
-    var secondButtonColor: Color
-    var thirdButtonColor: Color
+    /// The color of the main button.
+    let mainIconColor: Color
+    /// The color of the first expandable button.
+    let firstButtonIconColor: Color
+    /// The color of the second expandable button.
+    let secondButtonIconColor: Color
+    /// The color of the third expandable button.
+    let thirdButtonIconColor: Color
 
     // MARK: Public Initializer
+    /**
+     Initializes an ExpandableFloatingButton with customizable options.
+
+     - Parameters:
+       - usesGradient: A Boolean value that indicates whether to use gradient backgrounds. Default is `false`.
+       - mainIconName: The system name for the main button icon. Default is `"plus"`.
+       - firstButtonIconName: The system name for the first expandable button icon. Default is `"person.fill"`.
+       - secondButtonIconName: The system name for the second expandable button icon. Default is `"bell.fill"`.
+       - thirdButtonIconName: The system name for the third expandable button icon. Default is `"square.and.arrow.up"`.
+       - mainIconColor: The color of the main button. Default is `.gray`.
+       - firstButtonIconColor: The color of the first expandable button. Default is `.red`.
+       - secondButtonIconColor: The color of the second expandable button. Default is `.blue`.
+       - thirdButtonIconColor: The color of the third expandable button. Default is `.green`.
+     */
     public init(
-        isGradient: Bool = true,
-        mainSystemName: String = "plus",
-        firstSystemName: String = "person.fill",
-        secondSystemName: String = "bell.fill",
-        thirdSystemName: String = "square.and.arrow.up",
-        mainButtonColor: Color = .gray,
-        firstButtonColor: Color = .red,
-        secondButtonColor: Color = .blue,
-        thirdButtonColor: Color = .green
+        usesGradient: Bool = false,
+        mainIconName: String = "plus",
+        firstButtonIconName: String = "person.fill",
+        secondButtonIconName: String = "bell.fill",
+        thirdButtonIconName: String = "square.and.arrow.up",
+        mainIconColor: Color = .gray,
+        firstButtonIconColor: Color = .red,
+        secondButtonIconColor: Color = .blue,
+        thirdButtonIconColor: Color = .green
     ) {
-        self.isGradient = isGradient
-        self.mainSystemName = mainSystemName
-        self.firstSystemName = firstSystemName
-        self.secondSystemName = secondSystemName
-        self.thirdSystemName = thirdSystemName
-        self.mainButtonColor = mainButtonColor
-        self.firstButtonColor = firstButtonColor
-        self.secondButtonColor = secondButtonColor
-        self.thirdButtonColor = thirdButtonColor
+        self.usesGradient = usesGradient
+        self.mainIconName = mainIconName
+        self.firstButtonIconName = firstButtonIconName
+        self.secondButtonIconName = secondButtonIconName
+        self.thirdButtonIconName = thirdButtonIconName
+        self.mainIconColor = mainIconColor
+        self.firstButtonIconColor = firstButtonIconColor
+        self.secondButtonIconColor = secondButtonIconColor
+        self.thirdButtonIconColor = thirdButtonIconColor
     }
 
     public var body: some View {
-        ButtonView(action: {}, systemName: firstSystemName, color: firstButtonColor, isPushed: $isPushed, isGradient: isGradient)
+        // Layout for expandable buttons and the main button.
+        ButtonView(action: {}, systemName: firstButtonIconName, color: firstButtonIconColor, isExpanded: $isExpanded, usesGradient: usesGradient)
             .shadow(radius: 8)
-            .offset(x: isPushed ? -64 : 0)
-            .opacity(isPushed ? 1 : 0)
+            .offset(x: isExpanded ? -64 : 0)
+            .opacity(isExpanded ? 1 : 0)
 
-        ButtonView(action: {}, systemName: secondSystemName, color: secondButtonColor, isPushed: $isPushed, isGradient: isGradient)
+        // Second button.
+        ButtonView(action: {}, systemName: secondButtonIconName, color: secondButtonIconColor, isExpanded: $isExpanded, usesGradient: usesGradient)
             .shadow(radius: 8)
-            .offset(x: isPushed ? -56 : 0, y: isPushed ? -56 : 0)
-            .opacity(isPushed ? 1 : 0)
+            .offset(x: isExpanded ? -56 : 0, y: isExpanded ? -56 : 0)
+            .opacity(isExpanded ? 1 : 0)
 
-        ButtonView(action: {}, systemName: thirdSystemName, color: thirdButtonColor, isPushed: $isPushed, isGradient: isGradient)
+        // Third button.
+        ButtonView(action: {}, systemName: thirdButtonIconName, color: thirdButtonIconColor, isExpanded: $isExpanded, usesGradient: usesGradient)
             .shadow(radius: 8)
-            .offset(y: isPushed ? -64 : 0)
-            .opacity(isPushed ? 1 : 0)
+            .offset(y: isExpanded ? -64 : 0)
+            .opacity(isExpanded ? 1 : 0)
 
+        // Main button with toggle action.
         ButtonView(action: {
             withAnimation(.spring(response: 0.5, dampingFraction: 0.4, blendDuration: 0.2)) {
-                isPushed.toggle()
+                isExpanded.toggle()
             }
-        }, systemName: mainSystemName, color: mainButtonColor, isPushed: $isPushed, isGradient: isGradient)
+        }, systemName: mainIconName, color: mainIconColor, isExpanded: $isExpanded, usesGradient: usesGradient)
         .shadow(radius: 4)
-        .rotationEffect(.degrees(isPushed ? 405 : 0))
-        .scaleEffect(isPushed ? 1.3 : 1)
+        .rotationEffect(.degrees(isExpanded ? 405 : 0))
+        .scaleEffect(isExpanded ? 1.3 : 1)
     }
 
+    /// A subview representing a single button in the floating button stack.
     struct ButtonView: View {
         // Arguments
         let action: () -> Void
@@ -76,12 +102,12 @@ public struct ExpandableFloatingButton: View {
         let color: Color
 
         // Internal Properties
-        @Binding var isPushed: Bool
-        let isGradient: Bool
+        @Binding var isExpanded: Bool
+        let usesGradient: Bool
 
         public var body: some View {
-            if isGradient {
-                Button(action: {action()}, label: {
+            if usesGradient {
+                Button(action: { action() }, label: {
                     Image(systemName: systemName)
                         .padding()
                         .foregroundStyle(.white)
@@ -92,7 +118,7 @@ public struct ExpandableFloatingButton: View {
                         )
                 })
             } else {
-                Button(action: {action()}, label: {
+                Button(action: { action() }, label: {
                     Image(systemName: systemName)
                         .padding()
                         .foregroundStyle(.white)
@@ -103,8 +129,6 @@ public struct ExpandableFloatingButton: View {
                         )
                 })
             }
-
         }
     }
 }
-
